d2eb62f0fc161fd83477338078d5380491d2b28e
ddren <ddren@LAPTOP-F0MJINGF.modem>
2020-11-24 13:05:24 +1100

    In Class1111

--- src/fxanimationeffects/FxAnimationEffects.java
+++ src/fxanimationeffects/FxAnimationEffects.java
@@ -31,7 +31,7 @@ public class FxAnimationEffects extends Application {
       
         //Creating Rectangle   
         Rectangle rect = new Rectangle(200,100,200,200);  
-        rect.setFill(Color.BLUE);  // update colour
+        rect.setFill(Color.RED);  // update colour
         rect.setStroke(Color.GREEN);  //Update colour
         rect.setStrokeWidth(5);  
           
